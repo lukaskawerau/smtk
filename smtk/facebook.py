596904a3ae1f8f8549b05d46853429ff135363ed
Lukas Kawerau <lukas.kawerau@gmail.com>
2017-04-11 16:44:23 -0700

    Add get_comments functionality
    This is basically a straight port from collect_social, I just renamed some function calls to follow the conventions of smtk and wrote a bit of documentation.

--- smtk/facebook.py
+++ smtk/facebook.py
@@ -1,7 +1,6 @@
 import facepy
 from smtk.utils import helpers
-
-from smtk.utils import helpers
+import datetime
 
 
 class CollectFacebook():
@@ -18,6 +17,39 @@ class CollectFacebook():
 
     def on_comment(self):
         """Called when comment is found"""
+        comments = db['comment']
+        comment = comments.find_one(comment_id=comment_data['id'])
+        today = datetime.datetime.today()
+
+        if not comment:
+            fields = [
+                'comment_count',
+                'created_time',
+                'like_count',
+                'message'
+            ]
+
+            data = {
+                'post_id': post_id,
+                'comment_id': comment_data['id'],
+                'first_scraped': today
+
+            }
+
+            if parent:
+                data['parent_id'] = parent
+
+            author_id = None
+            if 'from' in comment_data and 'id' in comment_data['from']:
+                data['author_id'] = comment_data['from']['id']
+                author_id = data['author_id']
+                on_profile(db, comment_data['from'])
+
+            for f in fields:
+                if f in comment_data:
+                    data[f] = comment_data[f]
+
+            comments.insert(data, ensure=True)
         pass
 
     def on_post(self):
@@ -39,12 +71,50 @@ class CollectFacebook():
         # verify credentials
         pass
 
-    def get_comments(self):
-        # accepts list of post IDs
-        # calls on_comment for each comment returned
-        # param options: max comments per post
-        # see
-        # https://github.com/Data4Democracy/collect-social/blob/master/collect_social/facebook/get_comments.py
+    def get_comments(self, db=None, post_id=None, after=None, parent=None, max_comments=5000):
+        """
+        Accepts a list of post IDs.
+        Calls `on_comment` for each comment returned.
+        :param max_comments: the maximum number of comments that should be gathered. Works in reverse chronological order: the newest max_comments comments are downloaded. Default is 5000.
+        """
+        limit = 100
+
+        kwargs = {
+            'path': '/' + str(post_id) + '/comments',
+            'limit': limit
+        }
+
+        if after:
+            kwargs['after'] = after
+
+        try:
+            post_data = self.graph.get(**kwargs)
+            post_comments = post_data['data']
+
+            for comment in post_comments:
+                on_comment(db, comment, post_id, parent=parent)
+
+                if not parent:
+                    i += 1
+                    if i % 100 == 0:
+                        print('Finished ' + str(i) + ' comments')
+
+                    if i > max_comments:
+                        return
+
+                if not parent and 'comment_count' in comment and \
+                        comment['comment_count'] > 0:
+                    get_comments(graph, db, comment['id'], parent=comment['id'])
+
+            if len(post_comments) == limit:
+                _after = post_data['paging']['cursors']['after']
+
+                time.sleep(1)
+                get_comments(graph, db, post_id, i=i, after=_after, parent=parent)
+        except Exception as e:
+            print(e)
+            pass
+
         pass
 
     def get_posts(self, db=None, page_id=None, max_posts=None, date_range=None):
